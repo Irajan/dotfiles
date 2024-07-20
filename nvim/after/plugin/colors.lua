1ce6346f25ccea3a6c337f9db5e86fb392fb0502
irajan <irajan.dhakal@gmail.com>
2024-07-20 12:05:29 +0545

    Update to latest nvim plugins

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,60 +1,60 @@
 local ok, rosePine = pcall(require, "rose-pine")
 if not ok then
-	print("Failed to setup rose pine theme")
-	return
+  print("Failed to setup rose pine theme")
+  return
 end
 
 rosePine.setup({
-	--- @usage 'auto'|'main'|'moon'|'dawn'
-	variant = "moon",
-	--- @usage 'main'|'moon'|'dawn'
-	dark_variant = "auto",
-	bold_vert_split = false,
-	dim_nc_background = true,
-	disable_background = false,
-	disable_float_background = false,
-	disable_italics = false,
+  --- @usage 'auto'|'main'|'moon'|'dawn'
+  variant = "moon",
+  --- @usage 'main'|'moon'|'dawn'
+  dark_variant = "auto",
+  bold_vert_split = false,
+  dim_nc_background = true,
+  disable_background = false,
+  disable_float_background = false,
+  disable_italics = false,
 
-	--- @usage string hex value or named color from rosepinetheme.com/palette
-	groups = {
-		background = "base",
-		background_nc = "nc",
-		panel = "surface",
-		panel_nc = "base",
-		border = "highlight_med",
-		comment = "muted",
-		link = "iris",
-		punctuation = "subtle",
+  --- @usage string hex value or named color from rosepinetheme.com/palette
+  groups = {
+    background = "base",
+    background_nc = "nc",
+    panel = "surface",
+    panel_nc = "base",
+    border = "highlight_med",
+    comment = "muted",
+    link = "iris",
+    punctuation = "subtle",
 
-		error = "love",
-		hint = "iris",
-		info = "foam",
-		warn = "gold",
+    error = "love",
+    hint = "iris",
+    info = "foam",
+    warn = "gold",
 
-		headings = {
-			h1 = "iris",
-			h2 = "foam",
-			h3 = "rose",
-			h4 = "gold",
-			h5 = "pine",
-			h6 = "foam",
-		},
-	},
+    headings = {
+      h1 = "iris",
+      h2 = "foam",
+      h3 = "rose",
+      h4 = "gold",
+      h5 = "pine",
+      h6 = "foam",
+    },
+  },
 
-	-- Change specific vim highlight groups
-	-- https://github.com/rose-pine/neovim/wiki/Recipes
-	highlight_groups = {
-		ColorColumn = { bg = "pine" },
+  -- Change specific vim highlight groups
+  -- https://github.com/rose-pine/neovim/wiki/Recipes
+  highlight_groups = {
+    ColorColumn = { bg = "pine" },
 
-		-- Blend colours against the "base" background
-		CursorLine = { bg = "base", fg = "iris", blend = 10 },
-		StatusLine = { fg = "foam", bg = "rose", blend = 10 },
+    -- Blend colours against the "base" background
+    CursorLine = { bg = "base", fg = "iris", blend = 10 },
+    StatusLine = { fg = "foam", bg = "rose", blend = 10 },
 
-		-- By default each group adds to the existing config.
-		-- If you only want to set what is written in this config exactly,
-		-- you can set the inherit option:
-		Search = { bg = "gold", inherit = false },
-	},
+    -- By default each group adds to the existing config.
+    -- If you only want to set what is written in this config exactly,
+    -- you can set the inherit option:
+    Search = { bg = "gold", inherit = false },
+  },
 })
 
 -- Set colorscheme after options
@@ -62,26 +62,27 @@ vim.opt.guicursor = "n-v-c:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor
 vim.cmd("colorscheme rose-pine")
 
 function ClearBackground()
-	vim.api.nvim_set_hl(0, "CmdLine", { fg = "#000000", bg = "#000000" })
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	vim.api.nvim_set_hl(0, "Cursor", { fg = "#ffffff", bg = "#ffffff" })
+  vim.api.nvim_set_hl(0, "CmdLine", { fg = "#000000", bg = "#000000" })
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "Cursor", { fg = "#ffffff", bg = "#ffffff" })
 end
+
 ClearBackground()
 
 function MakeThemeDark()
-	-- TODO Swith to dark theme
-	vim.cmd("colorscheme rose-pine-moon")
-	vim.api.nvim_set_hl(0, "Normal", { bg = "black", fg = "#ffffff" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "black" })
-	vim.api.nvim_set_hl(0, "Cursor", { fg = "#ffffff", bg = "#ffffff" })
+  -- TODO Swith to dark theme
+  vim.cmd("colorscheme rose-pine-moon")
+  vim.api.nvim_set_hl(0, "Normal", { bg = "black", fg = "#ffffff" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "black" })
+  vim.api.nvim_set_hl(0, "Cursor", { fg = "#ffffff", bg = "#ffffff" })
 end
 
 function MakeThemeLight()
-	vim.cmd("colorscheme rose-pine-dawn")
-	vim.api.nvim_set_hl(0, "Normal", { bg = "white", fg = "#000000" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "white" })
-	vim.api.nvim_set_hl(0, "Cursor", { fg = "#000000", bg = "#000000" })
+  vim.cmd("colorscheme rose-pine-dawn")
+  vim.api.nvim_set_hl(0, "Normal", { bg = "white", fg = "#000000" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "white" })
+  vim.api.nvim_set_hl(0, "Cursor", { fg = "#000000", bg = "#000000" })
 end
 
 vim.keymap.set("n", "<A-c>", ClearBackground, {})
--- nvim/lua/plugins/setup.lua
+++ nvim/lua/plugins/setup.lua
@@ -1,117 +1,127 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	--- Colour scheme
-	"rose-pine/neovim",
+  --- Colour scheme
+  "rose-pine/neovim",
 
-	-- Telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.4",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-			"nvim-telescope/telescope-live-grep-args.nvim",
-			"AckslD/nvim-neoclip.lua",
-			"nvim-telescope/telescope-ui-select.nvim",
-			"debugloop/telescope-undo.nvim",
-			{
-				"aaronhallaert/advanced-git-search.nvim",
-				dependencies = {
-					"tpope/vim-fugitive",
-					"tpope/vim-rhubarb",
-				},
-			},
-		},
-	},
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.4",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      "AckslD/nvim-neoclip.lua",
+      "nvim-telescope/telescope-ui-select.nvim",
+      "debugloop/telescope-undo.nvim",
+      {
+        "aaronhallaert/advanced-git-search.nvim",
+        dependencies = {
+          "tpope/vim-fugitive",
+          "tpope/vim-rhubarb",
+        },
+      },
+    },
+  },
 
-	-- Treesitter
-	"nvim-treesitter/nvim-treesitter",
+  -- Treesitter
+  "nvim-treesitter/nvim-treesitter",
 
-	-- LSP
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v2.x",
-		dependencies = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" },
-			"williamboman/mason.nvim",
-			{ "williamboman/mason-lspconfig.nvim" },
+  -- LSP
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v2.x",
+    dependencies = {
+      -- LSP Support
+      { "neovim/nvim-lspconfig" },
+      "williamboman/mason.nvim",
+      { "williamboman/mason-lspconfig.nvim" },
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "L3MON4D3/LuaSnip" },
-			{ "rafamadriz/friendly-snippets" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "hrsh7th/cmp-cmdline" },
-			{ "saadparwaiz1/cmp_luasnip" },
-		},
-	},
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "L3MON4D3/LuaSnip" },
+      { "rafamadriz/friendly-snippets" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "hrsh7th/cmp-cmdline" },
+      { "saadparwaiz1/cmp_luasnip" },
+    },
+  },
 
-	{
-		"stevearc/oil.nvim",
-		opts = {},
-		-- Optional dependencies
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
 
-	-- harpoon
-	{
-		"theprimeagen/harpoon",
-	},
+  -- harpoon
+  {
+    "theprimeagen/harpoon",
+  },
 
-	-- Lua line
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-	},
+  -- Lua line
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
 
-	-- Formatter
-	{
-		"stevearc/conform.nvim",
-	},
+  -- Formatter
+  {
+    "stevearc/conform.nvim",
+  },
 
-	-- Linting
-	{
-		"mfussenegger/nvim-lint",
-	},
+  -- Linting
+  {
+    "mfussenegger/nvim-lint",
+  },
 
-	-- Git workflow
-	{
-		"akinsho/toggleterm.nvim", -- toogle term for lazy git
-		"lewis6991/gitsigns.nvim",
-	},
+  -- Git workflow
+  {
+    "akinsho/toggleterm.nvim", -- toogle term for lazy git
+    "lewis6991/gitsigns.nvim",
+  },
 
-	-- Other productivity plugins
-	{
-		"kylechui/nvim-surround",
-		version = "*",
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end,
-	},
-	{
-		"kevinhwang91/nvim-bqf",
-	},
-	{
-		"windwp/nvim-autopairs",
-	},
+  -- Other productivity plugins
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
+  {
+    "kevinhwang91/nvim-bqf",
+  },
+  {
+    "windwp/nvim-autopairs",
+  },
+
+  -- Auto commenting plugin
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+      -- add any options here
+    }
+  }
+
+
 })
